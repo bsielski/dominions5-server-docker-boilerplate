e5e4073864a0de3cab6bfd07fa0836e812677d0b
Bartłomiej Sielski <b.sielski.webdev@gmail.com>
2020-03-20 21:20:25 +0100

    Fix service name

--- bash.sh
+++ bash.sh
@@ -8,6 +8,6 @@ echo "-------------------------------------"
 echo "Script '${SCRIPT_NAME}', step 1/1"
 echo "Bash in the container..."
 
-USER_ID="${USER_ID}" GROUP_ID="${GROUP_ID}" GAME_PORT=$GAME_PORT docker-compose -f "${COMPOSE_FILE}" exec game bash
+USER_ID="${USER_ID}" GROUP_ID="${GROUP_ID}" GAME_PORT=$GAME_PORT docker-compose -f "${COMPOSE_FILE}" exec dom5_server bash
 
 docker ps -a
